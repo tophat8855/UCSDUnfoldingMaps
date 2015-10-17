896155953a16e1c5fd4a904ecab7a5dfafacde12
tophat8855 <hrfarley@gmail.com>
2015-10-16 21:20:06 -0700

    create getter and setter

--- src/demos/SimpleLocation.java
+++ src/demos/SimpleLocation.java
@@ -10,21 +10,51 @@ package demos;
  */
 public class SimpleLocation
 {
-    public double latitude;
-    public double longitude;
+    private double latitude;
+    private double longitude;
     
     public SimpleLocation()
     {
         this.latitude = 32.9;
         this.longitude = -117.2;
     }
-   
+
     public SimpleLocation(double latIn, double lonIn)
     {
         this.latitude = latIn;
         this.longitude = lonIn;
     }
-    
+
+    public double getLatitude()
+    {
+        return this.latitude;
+    }
+
+    public double getLongitude()
+    {
+        return this.longitude;
+    }
+
+    public void setLatitude(double lat)
+    {
+        if(lat < -180 || lat > 180) {
+            System.out.println("Illegal value for latitude");
+        }
+        else {
+            this.latitude = lat;
+        }
+    }
+
+    public void setLongitude(double lon)
+    {
+        if(lon < -180 || lon > 180) {
+            System.out.println("Illegal value for longitude");
+        }
+        else {
+            this.longitude = lon;
+        }
+    }
+
     // Returns the distance in km between this SimpleLocation and the 
     // parameter other
     public double distance(SimpleLocation other)
